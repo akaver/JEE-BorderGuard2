703428005eac90c558b242996645604ed30072ec
akaver <akaver@akaver.com>
2013-01-18 09:36:30 +0200

    more jars, jars everywhere

--- src/border/repository/AdminUnitTypeRepositoryImpl.java
+++ src/border/repository/AdminUnitTypeRepositoryImpl.java
@@ -1,13 +1,17 @@
 package border.repository;
 
+import org.springframework.data.jpa.repository.support.QueryDslJpaRepository;
+
 import border.model.AdminUnitType;
 
 public class AdminUnitTypeRepositoryImpl implements AdminUnitTypeRepositoryCustom{
 
+	private QueryDslJpaRepository<AdminUnitType, Long> repository;
+
 	@Override
 	public AdminUnitType findById(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return repository.findOne(id);
 	}
 
 }
